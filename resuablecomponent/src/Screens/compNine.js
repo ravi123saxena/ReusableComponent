f12a98e546710e3a0e24a96f24604214c96b4616
Ravindra Kumar Saxena <r.saxena@zensar.com>
2022-07-11 14:02:17 +0530

    bug fixes

--- resuablecomponent/src/Screens/compNine.js
+++ resuablecomponent/src/Screens/compNine.js
@@ -1,28 +1,27 @@
 import React, { useState } from 'react';
 import { View } from 'react-native';
 import HeaderBar from '../Components/Header';
-// import DisplayModalWithTextInput from '../Components/Modal/ModalWithTextInput';
-
+import { DisplayModalWithImage } from '../Components/Modal/ModalWithImage';
 
 export default CompNine = ({ navigation }) => {
     const [visible, setVisible] = useState(true)
-    const submitInput = (inputText) => {
-        console.log("sendInput (DialogInput#1): "+inputText);
-    }
+   
     return (
         <View style={{ flex: 1 }}>
             <HeaderBar
                 leftIconName={'chevron-left'}
                 text={'Modal With Image'}
                 handleLeftIconPress={() => navigation.goBack()} />
-            {/* <DisplayModalWithTextInput 
-                isDialogVisible={visible}
-                title={"Modal With Text Input"}
-                message={"This is a custom message"}
-                hintInput={"Enter Name"}
-                submitInput={(inputText) => { submitInput(inputText) }}
-                closeDialog={() => setVisible(false)}>
-            </DisplayModalWithTextInput> */}
+           <DisplayModalWithImage
+                display={visible}
+                message={'This is custom Modal with success'}
+                headingText={'Modal Success'}
+                buttonText={'Done'}
+                onConfirm={()=> setVisible(false)}
+                onClose={()=> setVisible(false)}
+                customModalContainerStyle={{height: 370}}
+                showImage
+           />
         </View>
     );
 };
